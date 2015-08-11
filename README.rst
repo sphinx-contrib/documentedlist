e5489c955593b17b63573779f4c912bbd92b2ff6
Chintalagiri Shashank <shashank@chintal.in>
2015-08-12 00:20:09 +0530

    Inital Commit

---
+++ README.rst
@@ -0,0 +1,61 @@
+
+DocumentedList Sphinx Extension
+===============================
+
+This file provides a Sphinx extension to convert a Python list into
+a table in the generated documentation. The intended application of
+this extension is to document the items of essentially list-like
+objects of immutable data (possibly enums, though python 3.4 enums
+are not supported yet).
+
+In the source code, each list item, instead of being just it's native
+data type, should be replaced by a tuple of two elements. The second
+element of the tuple should be a string which provides a description
+for the item.
+
+In the first use of this extension, the aim is to document a list of
+supported device classes (each of which is a string). This was
+originally specified in the python code as a list, and the same list
+with descriptions was manually maintained (or, in reality, left
+unmaintained) in the corresponding documentation. The list is now
+replaced with a tuple containing both the recognized strings and the
+description for each, and Sphinx is able to use this extension to
+'autogenerate' the table in the documentation.
+
+Based heavily on slides from Doug Hellman's PyCon talk,
+`http://www.slideshare.net/doughellmann/better-documentation-through-automation-creating-docutils-sphinx-extensions`_
+
+Installation & Usage
+--------------------
+
+To use this module, place the ``documentedlist.py`` file somewhere in
+the ``PYTHONPATH`` that Sphinx is configured to use, or add the git
+checkout folder to the ``PYTHONPATH`` in Sphinx's ``conf.py``.
+
+In the .rst file where the table should appear, insert the Sphinx
+directive provided by this module :
+
+    .. code-block:: rest
+
+        .. documentedlist::
+            :listobject: full.importable.path.of.the.list
+
+This extension will import the list as :
+
+    .. code-block:: python
+
+        from full.importable.path.of.the import list
+
+For a usage example, see:
+
+`https://github.com/chintal/koala/blob/public/koala/conventions/electronics.py`_
+
+and the corresponding documentation generated at:
+
+`http://koala.longclaw/doc/userdoc/conventions/gedasymbols.html#device-classes`_
+
+License
+-------
+
+This Sphinx Extension is made available under the BSD 3-clause License. See the
+LICENSE file for the full text.
---
+++ documentedlist.py
@@ -0,0 +1,96 @@
+
+from docutils import nodes
+from docutils.parsers.rst import directives
+from docutils.parsers.rst.directives.tables import Table
+
+
+class DocumentedListDirective(Table):
+
+    option_spec = {'listobject': directives.unchanged}
+
+    def run(self):
+        if self.content:
+            error = self.state_machine.reporter.error(
+                """The documentedlist directive does not know what to do with
+                the provided content""",
+                nodes.literal_block(self.block_text, self.block_text),
+                line=self.lineno
+            )
+            return [error]
+
+        # Get the list containing the documentation
+        memberpath = self.options.get('listobject', None)
+        if memberpath is None:
+            error = self.state_machine.reporter.error(
+                "The documentedlist needs to be given the list object"
+                "containing the documentations as the :listobject: parameter",
+                nodes.literal_block(self.block_text, self.block_text),
+                line=self.lineno
+            )
+            return [error]
+
+        try:
+            modstr, memberstr = memberpath.rsplit('.', 1)
+            mod = __import__(modstr, fromlist=[memberstr])
+            member = getattr(mod, memberstr)
+        except ImportError:
+            error = self.state_machine.reporter.error(
+                "Documentedlist encountered an error importing the member "
+                "specified by " + memberpath,
+                nodes.literal_block(self.block_text, self.block_text),
+                line=self.lineno
+            )
+            return [error]
+
+        table_headers = ['Item', 'Description']
+        table_body = member
+        max_cols = len(table_headers)
+
+        col_widths = self.get_column_widths(max_cols)
+
+        title, messages = self.make_title()
+        table_node = self.build_table(table_body,
+                                      col_widths,
+                                      table_headers)
+        self.add_name(table_node)
+        if title:
+            table_node.insert(0, title)
+        return [table_node] + messages
+
+    @staticmethod
+    def build_table(table_data, col_widths, headers):
+        table = nodes.table()
+        tgroup = nodes.tgroup(cols=len(headers))
+        table += tgroup
+
+        tgroup.extend(nodes.colspec(colwidth=col_width) for
+                      col_width in col_widths)
+
+        thead = nodes.thead()
+        tgroup += thead
+
+        row_node = nodes.row()
+        thead += row_node
+        row_node.extend(nodes.entry(h, nodes.paragraph(text=h))
+                        for h in headers)
+
+        tbody = nodes.tbody()
+        tgroup += tbody
+
+        rows = []
+        for row in table_data:
+            trow = nodes.row()
+            for cell in row:
+                entry = nodes.entry()
+                para = nodes.paragraph(text=unicode(cell))
+                entry += para
+                trow += entry
+            rows.append(trow)
+        tbody.extend(rows)
+
+        return table
+
+
+def setup(app):
+    app.add_directive('documentedlist', DocumentedListDirective)
+    return {'version': '0.1'}
