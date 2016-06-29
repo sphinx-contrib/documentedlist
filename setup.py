ac7c169ea3b27c94a323f7c367661d7b52ec2080
Chintalagiri Shashank <shashank@chintal.in>
2016-06-29 10:25:22 +0530

    Repository structure brought in line with that within the sphinx-contrib repository.

---
+++ setup.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+import os
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+requires = ['Sphinx>=0.6', 'six']
+
+setup(
+    name='sphinxcontrib-documentedlist',
+    version='0.4',
+    url='http://bitbucket.org/birkenfeld/sphinx-contrib',
+    download_url='http://pypi.python.org/pypi/sphinxcontrib-documentedlist',
+    license='BSD',
+    author='Chintalagiri Shashank',
+    author_email='shashank@chintal.in',
+    description='Sphinx DocumentedList extension',
+    long_description=read('README.rst'),
+    zip_safe=False,
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Framework :: Sphinx :: Extension',
+        'Topic :: Documentation',
+        'Topic :: Utilities',
+    ],
+    platforms='any',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=requires,
+    namespace_packages=['sphinxcontrib'],
+)
