73fa4df717416821f3888d3de88607195cd838c2
Chintalagiri Shashank <shashank@chintal.in>
2017-03-09 15:45:30 +0530

    Changed development status to stable.

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ setup(
     long_description=read('README.rst'),
     zip_safe=False,
     classifiers=[
-        'Development Status :: 4 - Beta',
+        'Development Status :: 5 - Production/Stable',
         'Environment :: Console',
         'Environment :: Web Environment',
         'Intended Audience :: Developers',
